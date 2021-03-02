5674dbf8c3b1dbae4ccdc88fea9c20056b5f08ba
diana camacho <dcam1901@gmail.com>
2021-03-02 01:18:33 -0600

    Current model for current user atttribute per request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_current_user, id: :user_signed_in?
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+
+  def set_current_user
+    # current_user comes from devise
+    # using it to set the current attribute of user per request
+    Current.user = current_user
   end
 end
---
+++ app/models/current.rb
@@ -0,0 +1,5 @@
+class Current < ActiveSupport::CurrentAttributes
+  # docs: https://api.rubyonrails.org/classes/ActiveSupport/CurrentAttributes.html
+
+  attribute :user
+end
